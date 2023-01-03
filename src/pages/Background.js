bd2e6d3224c6a2aa0f93ef8c05e6de27c5023f5a
mr.Rhenium <mr.rhenium75614@gmail.com>
2023-01-04 00:22:03 +0530

    Skill page is completed

--- src/pages/Background.js
+++ src/pages/Background.js
@@ -3,6 +3,7 @@ import "./../CSS Folder/Background.css";
 import MenuBar from "../components/MenuBar";
 import Home from "./Home";
 import Project from "./Project";
+import Skill from "./Skill";
 
 const Background = () => {
   return (
@@ -10,6 +11,7 @@ const Background = () => {
       <div className="background_container">
         <Home />
         <Project />
+        <Skill />
       </div>
       <MenuBar />
     </>
---
+++ src/pages/Skill.js
@@ -0,0 +1,202 @@
+import React from "react";
+import "./../CSS Folder/Skill.css";
+
+const Skill = () => {
+  return (
+    <>
+      <section className="skill_page">
+        <section className="section_left">
+          <div className="upper_part">
+            <h2>SKILLS</h2>
+          </div>
+          <div className="lower_part">
+            <div className="card">
+              <div className="head">
+                <span className="img">
+                  <img src="./assets/light bulb.png" alt="skill_img" />
+                </span>
+                <span className="title">
+                  <p>HTML : Hyper Text Markup Language</p>
+                </span>
+              </div>
+              <div className="body">
+                <div className="item_cntnr">
+                  <div className="items">
+                    <span className="rods"></span>
+                    <span className="content">
+                      <span className="img">
+                        <img src="./assets/light bulb.png" alt="valid" />
+                      </span>
+                      <span className="points">
+                        <p>Understanding of All Elements</p>
+                      </span>
+                    </span>
+                  </div>
+                  {/* dummy */}
+                  <div className="items">
+                    <span className="rods"></span>
+                    <span className="content">
+                      <span className="img">
+                        <img src="./assets/light bulb.png" alt="valid" />
+                      </span>
+                      <span className="points">
+                        <p>All Elements</p>
+                      </span>
+                    </span>
+                  </div>
+                  <div className="items">
+                    <span className="rods"></span>
+                    <span className="content">
+                      <span className="img">
+                        <img src="./assets/light bulb.png" alt="valid" />
+                      </span>
+                      <span className="points">
+                        <p>Understanding</p>
+                      </span>
+                    </span>
+                  </div>
+                  <div className="items">
+                    <span className="rods"></span>
+                    <span className="content">
+                      <span className="img">
+                        <img src="./assets/light bulb.png" alt="valid" />
+                      </span>
+                      <span className="points">
+                        <p>Basic knowledge</p>
+                      </span>
+                    </span>
+                  </div>
+                  <div className="items">
+                    <span className="rods"></span>
+                    <span className="content">
+                      <span className="img">
+                        <img src="./assets/light bulb.png" alt="valid" />
+                      </span>
+                      <span className="points">
+                        <p>Elements</p>
+                      </span>
+                    </span>
+                  </div>
+                  <div className="items">
+                    <span className="rods"></span>
+                    <span className="content">
+                      <span className="img">
+                        <img src="./assets/light bulb.png" alt="valid" />
+                      </span>
+                      <span className="points">
+                        <p>3 stars</p>
+                      </span>
+                    </span>
+                  </div>
+                  {/*  */}
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+        <section className="section_right">
+          <div className="upper_part">
+            <h2>SKILLS</h2>
+          </div>
+          <div className="lower_part">
+            <div className="skill_items_cntnr">
+              <span className="skill_item active">
+                <span className="img">
+                  <img src="./assets/light bulb.png" alt="" />
+                </span>
+                <span className="title">
+                  <p>HTML</p>
+                </span>
+                <span className="btn">
+                  <button>More</button>
+                </span>
+              </span>
+              {/* dummy */}
+              <span className="skill_item">
+                <span className="img">
+                  <img src="./assets/light bulb.png" alt="" />
+                </span>
+                <span className="title">
+                  <p>HTML</p>
+                </span>
+                <span className="btn">
+                  <button>More</button>
+                </span>
+              </span>
+              <span className="skill_item">
+                <span className="img">
+                  <img src="./assets/light bulb.png" alt="" />
+                </span>
+                <span className="title">
+                  <p>HTML</p>
+                </span>
+                <span className="btn">
+                  <button>More</button>
+                </span>
+              </span>
+              <span className="skill_item">
+                <span className="img">
+                  <img src="./assets/light bulb.png" alt="" />
+                </span>
+                <span className="title">
+                  <p>HTML</p>
+                </span>
+                <span className="btn">
+                  <button>More</button>
+                </span>
+              </span>
+              <span className="skill_item">
+                <span className="img">
+                  <img src="./assets/light bulb.png" alt="" />
+                </span>
+                <span className="title">
+                  <p>HTML</p>
+                </span>
+                <span className="btn">
+                  <button>More</button>
+                </span>
+              </span>
+              <span className="skill_item">
+                <span className="img">
+                  <img src="./assets/light bulb.png" alt="" />
+                </span>
+                <span className="title">
+                  <p>HTML</p>
+                </span>
+                <span className="btn">
+                  <button>More</button>
+                </span>
+              </span>
+              <span className="skill_item">
+                <span className="img">
+                  <img src="./assets/light bulb.png" alt="" />
+                </span>
+                <span className="title">
+                  <p>HTML</p>
+                </span>
+                <span className="btn">
+                  <button>More</button>
+                </span>
+              </span>
+              <span className="skill_item">
+                <span className="img">
+                  <img src="./assets/light bulb.png" alt="" />
+                </span>
+                <span className="title">
+                  <p>HTML</p>
+                </span>
+                <span className="btn">
+                  <button>More</button>
+                </span>
+              </span>
+              {/*  */}
+            </div>
+          </div>
+        </section>
+        <section className="section_menu"></section>
+      </section>
+    </>
+  );
+};
+
+export default Skill;
